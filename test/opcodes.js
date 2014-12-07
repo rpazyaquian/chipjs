a4470063f4024bc8c222300362ca6279394a45e7
Rebecca Paz <r.pazyaquian@gmail.com>
2014-12-07 14:04:38 -0500

    slight changes

--- test/opcodes.js
+++ test/opcodes.js
@@ -3,47 +3,117 @@ var should = require("should");
 describe('Chip8', function () {
   describe('#execute', function() {
     describe('0---', function () {
-      describe('NNN');
-      describe('0E0');
-      describe('0EE');
+      describe('NNN', function () {
+
+      });
+      describe('0E0', function () {
+
+      });
+      describe('0EE', function () {
+
+      });
     });
-    describe('1NNN');
-    describe('2NNN');
-    describe('3XNN');
-    describe('4XNN');
-    describe('5XY0');
-    describe('6XNN');
-    describe('7XNN');
+    describe('1NNN', function () {
+
+    });
+    describe('2NNN', function () {
+
+    });
+    describe('3XNN', function () {
+
+    });
+    describe('4XNN', function () {
+
+    });
+    describe('5XY0', function () {
+
+    });
+    describe('6XNN', function () {
+
+    });
+    describe('7XNN', function () {
+
+    });
     describe('8XY-', function () {
-      describe('0');
-      describe('1');
-      describe('2');
-      describe('3');
-      describe('4');
-      describe('5');
-      describe('6');
-      describe('7');
-      describe('E');
+      describe('0', function () {
+
+      });
+      describe('1', function () {
+
+      });
+      describe('2', function () {
+
+      });
+      describe('3', function () {
+
+      });
+      describe('4', function () {
+
+      });
+      describe('5', function () {
+
+      });
+      describe('6', function () {
+
+      });
+      describe('7', function () {
+
+      });
+      describe('E', function () {
+
+      });
     });
-    describe('9XY0');
-    describe('ANNN');
-    describe('BNNN');
-    describe('CXNN');
-    describe('DXYN');
+    describe('9XY0', function () {
+
+    });
+    describe('ANNN', function () {
+
+    });
+    describe('BNNN', function () {
+
+    });
+    describe('CXNN', function () {
+
+    });
+    describe('DXYN', function () {
+
+    });
     describe('EX--', function () {
-      describe('9E');
-      describe('A1');
+      describe('9E', function () {
+
+      });
+      describe('A1', function () {
+
+      });
     });
     describe('FX--', function () {
-      describe('07');
-      describe('0A');
-      describe('15');
-      describe('18');
-      describe('1E');
-      describe('29');
-      describe('33');
-      describe('55');
-      describe('65');
+      describe('07', function () {
+
+      });
+      describe('0A', function () {
+
+      });
+      describe('15', function () {
+
+      });
+      describe('18', function () {
+
+      });
+      describe('1E', function () {
+
+      });
+      describe('29', function () {
+
+      });
+      describe('33', function () {
+
+      });
+      describe('55', function () {
+
+      });
+      describe('65', function () {
+
+      });
     });
   });
 });
