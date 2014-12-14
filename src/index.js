86c8020a910e9a6ac1d95f876a09f64c6e1b17e5
Rebecca Paz <r.pazyaquian@gmail.com>
2014-12-14 15:25:29 -0500

    ready to define operations

--- src/index.js
+++ src/index.js
@@ -4,17 +4,18 @@ var blankRam = _und.map(_und.range(4096), function () {
   return 0;
 });
 
-var blankDisplay = _und.map(_und.range(32, function () {
+var blankDisplay = _und.map(_und.range(32), function () {
   _und.map(_und.range(64), function () {
     return 0;
   });
-}));
+});
 
 var ChipJS = function() {
+
   // properties
-  this.ram: _und.clone(blankRam),
-  this.i: 0x000,
-  this.registers: [
+  this.ram = _und.clone(blankRam);
+  this.i = 0x000;
+  this.registers = [
     0x0,
     0x0,
     0x0,
@@ -31,26 +32,27 @@ var ChipJS = function() {
     0x0,
     0x0,
     0x0
-  ],
-  this.v: this.registers,
-  this.programCounter: 0x200,
-  this.stack: [],
-  this.display: blankDisplay,
-  this.soundTimer: 0x00,
-  this.delayTimer: 0x00,
+  ];
+  this.v = this.registers;
+  this.programCounter = 0x200;
+  this.stack = [];
+  this.display = blankDisplay;
+  this.soundTimer = 0x00;
+  this.delayTimer = 0x00;
   // utility methods
-  execute: function() {
-    // get operation matching opcode
-    this.operation = this.getOperation();
-    // execute this operation
-    this.operation();
-  },
-  getOperation: function() {
+  this.execute = function() {
+    // get opcode
     this.opcode = this.getOpcode();
-    var operation = this.translateOpcode(this.opcode);
+    // get operation matching opcode
+    var operation = this.getOperation(this.opcode);
+    // execute this operation given opcode parameters
+    operation(this.opcode);
+  },
+  this.getOperation = function(opcode) {
+    var operation = this.translateOpcode(opcode);
     return operation;
   },
-  getOpcode: function () {
+  this.getOpcode = function () {
     // opcode is defined as 0xNNNN, where
     // the first half is the current location of
     // the program counter, the second is
@@ -71,15 +73,17 @@ var ChipJS = function() {
 
     return opcode;
   },
-  mergeBytes: function(head, tail) {
+  this.getAddress = function(full) {
+    return (full & 0x0FFF);
+  },
+  this.mergeBytes = function(head, tail) {
     return ((head << 8) | tail);
   },
-  translateOpcode: function(opcode) {
-    var digits = this.opcode.digits;
-    var operation = this.matchDigits(digits);
+  this.translateOpcode = function(opcode) {
+    var operation = this.matchDigits(opcode);
     return operation;
   },
-  splitIntoDigits: function(full) {
+  this.splitIntoDigits = function(full) {
     var full = full;
     var digits = _und.map([12, 8, 4, 0], function (bits) {
       // shift 0xABCD right by 0 digits
@@ -90,75 +94,79 @@ var ChipJS = function() {
     });
     return digits;
   },
-  matchDigits: function(digits) {
+  this.matchDigits = function() {
+    // this is a disgusting mess
+    // and i feel the need
+    // to apologize for it
     var self = this;
-    // create a switch case
-    // and return a particular function
-    // based on the first digit
-    // operate recursively for sub-opcodes...
-
-    // fuck switch cases!
-    // these are integers, aren't they?
-    // make an array of arrays!
-
-    // the opcodes:
+    var digits = this.opcode.digits;
 
     var opcodes = [
-      // this is a disgusting mess
-      // and i feel the need
-      // to apologize for it
-      [
-        {
-          0xE0: self.clearScreen,  // 00E0
-          0xEE: self.returnFromSubroutine
-        }
-      ],  // 0---
-      [self.jumpToAddress],  // 1---
-      [self.executeSubroutine],  // 2---
-      [self.skipIfVXEqual],  // 3---
-      [self.skipIfVXNotEqual],  // 4---
-      [self.skipIfVXVYEqual],  // 5---
-      [self.storeToVX],  // 6---
-      [self.addToVX],  // 7---
-      [],  // 8--- (how am i gonna store this w/o strings?)
-      [self.skipIfVXVYNotEqual],  // 9---
-      [self.storeAddressToI],  // A---
-      [self.jumpToAddressV0],  // B---
-      [self.setVXToRandom],  // C---
-      [self.drawSprite],  // D---
-      [  // E---
-        {
-          0x9E: self.skipIfKeyPressed,
-          0xA1: self.skipIfKeyNotPressed
-        }
-      ],
-      [
-        {
-          0x07: self.storeDelayTimerToVX,
-          0x0A: self.storeKeypressToVX,
-          0x15: self.setDelayTimerToVX,
-          0x18: self.setSoundTimerToVX,
-          0x1E: self.addVXToI,
-          0x29: self.setIToFontDigit,
-          0x33: self.storeBCDOfVx,
-          0x55: self.storeRegistersToMemory,
-          0x65: self.loadRegistersFromMemory
-        }
-      ]   // F---
+      {
+        0xE0: self.clearScreen,  // 00E0
+        0xEE: self.returnFromSubroutine  // 00EE
+      }, // 0---
+      self.jumpToAddress,  // 1---
+      self.executeSubroutine,  // 2---
+      self.skipIfVXEqual,  // 3---
+      self.skipIfVXNotEqual,  // 4---
+      self.skipIfVXVYEqual,  // 5---
+      self.storeToVX,  // 6---
+      self.addToVX,  // 7---
+      {
+        0x00: self.storeVYToVX,
+        0x01: self.setVXToVXOrVY,
+        0x02: self.setVXToVXAndVY,
+        0x03: self.setVXToVXXorVY,
+        0x04: self.addVYToVX,
+        0x05: self.subtractVYFromVX,
+        0x06: self.storeRightShiftVYToVX,
+        0x07: self.setVXtoVYMinusVX,
+        0x0E: self.storeLeftShiftVYToVX
+      },  // 8---
+      self.skipIfVXVYNotEqual,  // 9---
+      self.storeAddressToI,  // A---
+      self.jumpToAddressV0,  // B---
+      self.setVXToRandom,  // C---
+      self.drawSprite,  // D---
+      {
+        0x9E: self.skipIfKeyPressed,
+        0xA1: self.skipIfKeyNotPressed
+      },  // E---
+      {
+        0x07: self.storeDelayTimerToVX,
+        0x0A: self.storeKeypressToVX,
+        0x15: self.setDelayTimerToVX,
+        0x18: self.setSoundTimerToVX,
+        0x1E: self.addVXToI,
+        0x29: self.setIToFontDigit,
+        0x33: self.storeBCDOfVx,
+        0x55: self.storeRegistersToMemory,
+        0x65: self.loadRegistersFromMemory
+      }  // F---
     ];
 
-    switch digits[0] {
+    var operation;
+
+    switch (digits[0]) {
       case 0x0:
+        operation = opcodes[digits[0]][this.opcode.tail];
         break;
       case 0x8:
+        operation = opcodes[digits[0]][digits[3]];
         break;
       case 0xE:
+        operation = opcodes[digits[0]][this.opcode.tail];
         break;
       case 0xF:
+        operation = opcodes[digits[0]][this.opcode.tail];
         break;
       default:
+        operation = opcodes[digits[0]];
         break;
     }
+
+    return operation;
 
   }
 };
